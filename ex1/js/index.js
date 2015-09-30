d3b86e6ec556f283bfb61198a450fc748ea0c291
sgnl <rayrfarias@gmail.com>
2015-09-29 19:50:18 -1000

    added ex2 and final boss

--- ex1/js/index.js
+++ ex1/js/index.js
@@ -1,4 +1,6 @@
 window.onload = function () {
-  // add an event listener to each li element inside of the nav menu
+  // add an event listener to each li element inside of the nav menu.
+  // Make it listen for a Click event
     // this event should log to the console the text inside of the <a> tag
+    // the event should only log one word per click.
 };
---
+++ ex2/js/index.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  // add an event listener to the `child` class.
+  // inspect the .css file
+    // when a `child` class is clicked you code will add CSS styles which will show the
+    // secret toy element. Click on an element will only show that element's toy, no one elses.
+};
