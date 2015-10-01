03e46a487609c249554b60ee612cc1f5e7e76bce
Victor <viclee02@gmail.com>
2015-09-30 18:26:50 -1000

    adds eventlistener

--- ex1/js/index.js
+++ ex1/js/index.js
@@ -3,4 +3,26 @@ window.onload = function () {
   // Make it listen for a Click event
     // this event should log to the console the text inside of the <a> tag
     // the event should only log one word per click.
+   var navi = document.getElementsByClassName('navLink');
+   //console.log(navi);
+
+   for(var i = 0; i<navi.length; i++){
+    //console.log(navi[i]);
+    navi[i].addEventListener('click', function(){
+      console.log(this);
+    })
+   }
+
+
+
+   /* var navLink = document.getElementsByClassName('navLink');
+    //console.log(navLink);
+
+    for(var i =0; i<navLink.length; i++){
+      //console.log(navLink[i]);
+      navLink[i].addEventListener('click', function(){
+        console.log(this);
+      });
+    }
+*/
 };
